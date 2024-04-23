946d88d0754f2a1ad0b491b4a8ec0e81cbbe0f6c
MentalSep <chahidjava@hotmail.com>
2024-04-23 16:43:12 +0100

    test

--- 0x02-minimum_operations/0-minoperations.py
+++ 0x02-minimum_operations/0-minoperations.py
@@ -8,12 +8,27 @@ def minOperations(n):
     to break down a positive integer n into smaller positive integers
     using prime factorization.
     """
-    operations = 0
+    # operations = 0
 
-    for factor in range(2, n + 1):
-        while (n % factor == 0):
-            operations += factor
-            n = n // factor
-        factor += 1
+    # for factor in range(2, n + 1):
+    #     while (n % factor == 0):
+    #         operations += factor
+    #         n = n // factor
+    #     factor += 1
 
-    return (operations)
+    # return (operations)
+    if n <= 1:
+        return 0
+
+    operations = [0] * (n + 1)
+    for i in range(2, n + 1):
+        if n % i == 0:
+            operations[i] = i
+
+    for i in range(2, n + 1):
+        for j in range(2, i // 2 + 1):
+            if i % j == 0:
+                operations[i] = min(operations[i],
+                                    operations[j] + operations[i // j])
+
+    return operations[n]
