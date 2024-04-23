2960f12b87d8cabf18baae7ce9dab2608c9522dc
MentalSep <chahidjava@hotmail.com>
2024-04-23 16:33:12 +0100

    add task 0

---
+++ 0x02-minimum_operations/0-minoperations.py
@@ -0,0 +1,17 @@
+#!/usr/bin/pytohn3
+
+def minOperations(n):
+    """
+    This function calculates the minimum number of operations required
+    to break down a positive integer n into smaller positive integers
+    using prime factorization.
+    """
+    operations = 0
+
+    for factor in range(2, n + 1):
+        while (n % factor == 0):
+            operations += factor
+            n = n // factor
+        factor += 1
+
+    return (operations)
