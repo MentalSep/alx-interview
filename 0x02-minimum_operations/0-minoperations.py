ad557242656a3b6103c46f82d4661d44ede9dcc4
MentalSep <chahidjava@hotmail.com>
2024-04-28 16:58:14 +0100

    pycodestyle fix

--- 0x02-minimum_operations/0-minoperations.py
+++ 0x02-minimum_operations/0-minoperations.py
@@ -12,10 +12,10 @@ def minOperations(n):
     factor = 2
 
     while n > 1:
-      if n % factor == 0:
-        operations += factor
-        n //= factor
-      else:
-        factor += 1
+        if n % factor == 0:
+            operations += factor
+            n //= factor
+        else:
+            factor += 1
 
     return operations
