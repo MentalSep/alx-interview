35d30ffdce4e99c5c4a6dddf2e1d63c3d19c3c8a
MentalSep <chahidjava@hotmail.com>
2024-05-02 19:07:43 +0100

    prnting format cahgne

--- 0x03-log_parsing/0-stats.py
+++ 0x03-log_parsing/0-stats.py
@@ -29,14 +29,14 @@ def process_logs():
     """Process log lines from stdin and compute statistics"""
     total_file_size = 0
     status_codes = {
-        200: 0,
-        301: 0,
-        400: 0,
-        401: 0,
-        403: 0,
-        404: 0,
-        405: 0,
-        500: 0
+        '200': 0,
+        '301': 0,
+        '400': 0,
+        '401': 0,
+        '403': 0,
+        '404': 0,
+        '405': 0,
+        '500': 0
     }
     lines_processed = 0
 
