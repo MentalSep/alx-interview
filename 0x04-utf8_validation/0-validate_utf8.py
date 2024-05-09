4573a4952bf063d1e914387fed63f6154b44bdf1
MentalSep <chahidjava@hotmail.com>
2024-05-09 08:58:50 +0100

    add task 0

--- 0x04-utf8_validation/0-validate_utf8.py
+++ 0x04-utf8_validation/0-validate_utf8.py
@@ -7,18 +7,22 @@ def validUTF8(data):
     count = 0
 
     for byte in data:
+        mask = 1 << 7
+        while mask & byte:
+            count += 1
+            mask >>= 1
+
         if count == 0:
-            if byte >> 5 == 0b110:
-                count = 1
-            elif byte >> 4 == 0b1110:
-                count = 2
-            elif byte >> 3 == 0b11110:
-                count = 3
-            elif byte >> 7:
-                return False
-        else:
-            if byte >> 6 != 0b10:
-                return False
-            count -= 1
+            continue
+
+        if count == 1 or count > 4:
+            return False
+
+        # Check for the expected format 10xxxxxx after the initial byte
+        mask = (1 << 7) >> count
+        if mask & byte == 0:
+            return False
+
+        count -= 1
 
     return count == 0
