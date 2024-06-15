0012df115b0c553e5f1a3ff2d191464dc0eeb9f6
MentalSep <chahidjava@hotmail.com>
2024-06-15 23:20:19 +0100

    add task 0

--- 0x0A-primegame/0-prime_game.py
+++ 0x0A-primegame/0-prime_game.py
@@ -3,36 +3,36 @@
 
 
 def isWinner(x, nums):
-    """ Prime Game """
-    def is_prime(n):
-        """ Check if a number is prime"""
-        if n < 2:
-            return False
-        for i in range(2, int(n**0.5) + 1):
-            if n % i == 0:
-                return False
-        return True
+    if x < 1 or not nums:
+        return None
 
-    def get_winner(n):
-        """ Get the winner of the game"""
-        primes = [i for i in range(2, n + 1) if is_prime(i)]
-        if not primes:
-            return 'Ben'
-        return 'Maria' if len(primes) % 2 != 0 else 'Ben'
+    max_num = max(nums)
+
+    sieve = [True] * (max_num + 1)
+    sieve[0] = sieve[1] = False
+
+    p = 2
+    while (p * p <= max_num):
+        if sieve[p]:
+            for i in range(p * p, max_num + 1, p):
+                sieve[i] = False
+        p += 1
+
+    primes = [p for p in range(max_num + 1) if sieve[p]]
 
     maria_wins = 0
     ben_wins = 0
 
     for n in nums:
-        winner = get_winner(n)
-        if winner == 'Maria':
-            maria_wins += 1
-        else:
+        primes_count = sum(1 for prime in primes if prime <= n)
+        if primes_count % 2 == 0:
             ben_wins += 1
+        else:
+            maria_wins += 1
 
     if maria_wins > ben_wins:
-        return 'Maria'
+        return "Maria"
     elif ben_wins > maria_wins:
-        return 'Ben'
+        return "Ben"
     else:
         return None
